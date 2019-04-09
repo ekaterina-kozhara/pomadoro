7a6275b140a23e5c6317e67a1c12e23351107d96
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-04-09 13:20:28 +0300

    1

--- scripts.js
+++ scripts.js
@@ -8,23 +8,41 @@ function startTimer(e) {
 
   const input = e.target.querySelector('input')
   input.disabled = true
+  const key = Date.now()
+  localStorage.setItem(key, input.value)
 
   progress.style.width = '100vw'
   progress.style.transitionDuration =  WORK_TIME + 'ms'
 
   setTimeout(function() {
+
     input.disabled = false
     input.focus()
-    input.value = ''
-    
+
     progress.style.transitionDuration = ''
     progress.style.width = ''
 
     const listItem = document.createElement('li')
     listItem.innerText = input.value
     tasks.appendChild(listItem)
+
+    input.value = ''
   },  WORK_TIME)
+}
+
+function loadHistory() {
+  const historySize = localStorage.length
+  if (historySize > 0) {
+    for (let i = 0; i < historySize; i++) {      
+      const key = localStorage.key(i)
+      const taskName = localStorage.getItem(key)
+      const listItem = document.createElement('li')
+      listItem.innerText = taskName
+      tasks.appendChild(listItem)
+    }
+  }
 }
 
 
 form.onsubmit = startTimer
+loadHistory()
