6fcdd258885fb4cb91c9e4bb8551ed24908d524f
Ekaterina Kozhara <ekaterina.kozhara@yandex.ru>
2019-04-16 13:16:55 +0300

    2 page +

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,11 +1,38 @@
 const form = document.querySelector('form')
+const switchTo25 = document.querySelector('.switch.is-left')
+const switchTo5 = document.querySelector('.switch.is-right')
+let taskTime = 5000
+const sectionFirst = document.getElementById('timer-section')
+const sectionSecond = document.getElementById('stats-section')
+const statisticToggle = document.getElementById('statisticToggle')
+const footerImg = document.querySelector('footer > img')
 
-form.onsubmit =function startTimer(e) {
+switchTo25.onclick = function handleTimeSwitch() {
+  this.classList.toggle('is-active')
+  switchTo5.classList.toggle('is-active')
+  taskTime = 5000
+}
+
+switchTo5.onclick = function handleTimeSwitch() {
+  this.classList.toggle('is-active')
+  switchTo25.classList.toggle('is-active')
+  taskTime = 1000
+}
+
+form.onsubmit = function startTimer(e) {
   event.preventDefault()
 
   setTimeout(function () {
     const input = e.target.querySelector('input[type=text]')
 
     console.log(input.value)
-  }, 2000)
+  }, taskTime)
 }
+
+statisticToggle.onclick = function handleScreenToggle() {
+  sectionFirst.style.display = "none"
+  sectionSecond.style.display = "block"
+  footerImg.src = "images/todolist.svg"
+}
+
+// handleScreenToggle()
